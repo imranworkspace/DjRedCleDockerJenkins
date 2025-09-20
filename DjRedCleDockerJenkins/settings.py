4db299f354d7889eaae547701990136d6770ab4d
imranlatur24 <imranlatur24@gmail.com>
2025-09-20 19:11:56 +0530

    change entrypoint.sh file

--- DjRedCleDockerJenkins/settings.py
+++ DjRedCleDockerJenkins/settings.py
@@ -127,10 +127,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Celery Redis configs
-'''CELERY_BROKER_URL='redis://127.0.0.1:6379/123'
-CELERY_RESULT_BACKEND='redis://127.0.0.1:6379/123'
-'''
+CELERY_BROKER_URL='redis://localhost:6379/101'
+CELERY_RESULT_BACKEND='redis://localhost:6379/101'
+
 
 # for docker,jenkins
-CELERY_BROKER_URL='redis://redis:6379/0'
-CELERY_RESULT_BACKEND='redis://redis:6379/0' 
+'''CELERY_BROKER_URL='redis://redis:6379/0'
+CELERY_RESULT_BACKEND='redis://redis:6379/0'  '''
