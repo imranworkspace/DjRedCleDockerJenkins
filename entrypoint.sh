182df9bf4e6003c77425bc2664f94e9c0292b998
imranlatur24 <imranlatur24@gmail.com>
2025-09-20 18:54:44 +0530

    update dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,20 @@ set -e
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
+echo "Creating superuser..."
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = "test"
+password = "imrandell"
+email = "test@example.com"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created: username=test, password=imrandell")
+else:
+    print("Superuser 'test' already exists.")
+EOF
+
 echo "Starting Django server..."
 exec "$@"
