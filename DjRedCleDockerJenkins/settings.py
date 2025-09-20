87a52d843863d4bcace676fa34f05015cd31c2da
imranlatur24 <imranlatur24@gmail.com>
2025-09-20 18:28:59 +0530

    update dockerfile

--- DjRedCleDockerJenkins/settings.py
+++ DjRedCleDockerJenkins/settings.py
@@ -132,5 +132,5 @@ CELERY_RESULT_BACKEND='redis://127.0.0.1:6379/123'
 '''
 
 # for docker,jenkins
-CELERY_BROKER_URL='redis://redis:6379/0'
-CELERY_RESULT_BACKEND='redis://redis:6379/0' 
+CELERY_BROKER_URL='redis://redis:6380/0'
+CELERY_RESULT_BACKEND='redis://redis:6380/0' 
