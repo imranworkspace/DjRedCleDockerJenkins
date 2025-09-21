75252cfc1bb8bba93b531c478bf27d57fa1dc5d4
imranlatur24 <imranlatur24@gmail.com>
2025-09-21 13:32:00 +0530

    update redis

--- DjRedCleDockerJenkins/settings.py
+++ DjRedCleDockerJenkins/settings.py
@@ -127,10 +127,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Celery Redis configs
-CELERY_BROKER_URL='redis://127.0.0.1:6379/101'
+'''CELERY_BROKER_URL='redis://127.0.0.1:6379/101'
 CELERY_RESULT_BACKEND='redis://127.0.0.1:6379/101'
-
+'''
 
 # for docker,jenkins
-'''CELERY_BROKER_URL='redis://redis:6379/0'
-CELERY_RESULT_BACKEND='redis://redis:6379/0'  '''
+# CELERY_BROKER_URL='redis://redis:6379/0'
+CELERY_BROKER_URL = "redis://host.docker.internal:6379/0"
+# CELERY_RESULT_BACKEND='redis://redis:6379/0' 
+CELERY_RESULT_BACKEND="redis://host.docker.internal:6379/0"
