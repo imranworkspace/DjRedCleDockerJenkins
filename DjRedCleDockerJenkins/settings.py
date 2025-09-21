7bb6b02eb204d6e4465dc2276846c719a8ee3594
imranlatur24 <imranlatur24@gmail.com>
2025-09-21 15:50:32 +0530

    update redis

--- DjRedCleDockerJenkins/settings.py
+++ DjRedCleDockerJenkins/settings.py
@@ -132,8 +132,8 @@ CELERY_RESULT_BACKEND='redis://127.0.0.1:6379/101'
 '''
 
 # for docker,jenkins
-'''CELERY_BROKER_URL = "redis://redis:6379/1"
-CELERY_RESULT_BACKEND = "redis://redis:6379/1"'''
+CELERY_BROKER_URL = "redis://redis:6379/1"
+CELERY_RESULT_BACKEND = "redis://redis:6379/1"
 
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://127.0.0.1:6379/1")
-CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/1")
+# CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://127.0.0.1:6379/1")
+# CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/1")
